5e7581c21c7443777e8168c441f6cf82f4b1e5c6
KramSany <KramSany@KRAMSANY>
2022-10-25 00:12:51 +0300

    Добавьте файлы проекта.

---
+++ Pract6/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pract6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            Pair pair3 = new Pair(2, 4);
+            pair3++;
+            MessageBox.Show(pair3.ToString());
+        }
+
+
+
+        private void About(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Поливода А.А.\n" + "Практическая работа 5\n" + "Создать класс Pair (пара четных чисел). Создать необходимые методы и свойства. " +
+                "\r\nОпределить метод вычисления произведения чисел. Создать перегруженный метод " +
+                "\r\nдля вычисления произведения 2 пар чисел – (а, b) * (с, d) \n = (a * c) , (b * d).");
+        }
+
+        private void Start(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                var selectedPair = ListPair.SelectedItems;
+                if (selectedPair.Count == 1)
+                {
+                    Pair pair1 = ListPair.Items[ListPair.SelectedIndex] as Pair;
+                    Result.Text = pair1.Myltiply().ToString();
+                }
+                else
+                {
+                    Pair pair1 = selectedPair[0] as Pair;
+                    Pair pair2 = selectedPair[1] as Pair;
+                    Result.Text = pair2.Myltiply(pair1).ToString();
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void AddPair(object sender, RoutedEventArgs e)
+        {
+            Pair pair = new Pair(Convert.ToInt32(FirstValue.Text), Convert.ToInt32(SecondValue.Text));
+            ListPair.Items.Add(pair);
+        }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Pract6/Pair.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract6
+{
+    internal class Pair
+    {
+        private int _first;
+        private int _second;
+        public Pair(int first, int second)
+        {
+            First = first;
+            Second = second;
+        }
+
+        public int First
+        {
+            get => _first;
+            set
+            {
+                if (value % 2 != 0)
+                    throw new ArgumentException("Вы даун");
+
+                _first = value;
+            }
+        }
+        public int Second
+        {
+            get { return _second; }
+            set
+            {
+                if (value % 2 != 0)
+                    throw new ArgumentException("Вы даун");
+
+                _second = value;
+            }
+        }
+        public int Myltiply()
+        {
+            return First * Second;
+        }
+        public Pair Myltiply(Pair value)
+        {
+            return new Pair(First * value.First, Second * value.Second);
+        }
+
+        public static Pair operator ++(Pair pairOne)
+        {
+            Pair result = new Pair(pairOne.First + pairOne.Second, pairOne.Second);
+            return result;
+
+        }
+        public override string ToString()
+        {
+            return $"{First}, {Second}";
+        }
+
+    }
+}
